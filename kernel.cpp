c7618c69a96552943a3adc7bb1a72ad207a7a01d
ayoubarouche <aarouche2016@gmail.com>
2022-08-01 22:41:41 +0100

    adding the gdt functionnality

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,9 @@
 #include "types.h"
-
+#include "gdt.h"
 void printf(const char* str){
     static unsigned short* VideoMemory = (unsigned short*)0xb8000;
     for(int i = 0; str[i]!='\0'; i++){
         VideoMemory[i] = (VideoMemory[i] & 0xFF00)| str[i];
-
     }
 }
 
@@ -18,6 +17,8 @@ extern "C" void callConstructors()
     }
 }
 extern "C" void kernelMain(void* multiboot_structure , uint32_t magicnumber){
-    printf("HELLO WORLD i am arouche ayoub and this is my operating system");
+    printf("HELLO WORLD i am arouche ayoub and this is my operating system\n");
+    GlobalDescriptorTable gdt ; 
+
     while(1);
 }
--- types.h
+++ types.h
@@ -5,7 +5,7 @@
     typedef unsigned char uint8_t;
     
     typedef short int16_t;
-    typedef unsigned short unint16_t;
+    typedef unsigned short uint16_t;
     
     typedef int int32_t;
     typedef unsigned int uint32_t;
